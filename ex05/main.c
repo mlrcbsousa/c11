c75c73c5eeac2e74aaf8d96daf51d4ad1799970b
Manuel Sousa <mlrcbsousa@gmail.com>
2021-02-06 04:24:17 +0100

    Add error handling for invalid operations with 0.

--- ex05/main.c
+++ ex05/main.c
@@ -6,13 +6,13 @@
 /*   By: manuel <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/25 00:06:04 by manuel            #+#    #+#             */
-/*   Updated: 2021/02/06 04:06:12 by manuel           ###   ########.fr       */
+/*   Updated: 2021/02/06 04:23:32 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft.h"
 
-void	init(int (*f[5])(int, int))
+void	init(t_op f[5])
 {
 	f[0] = add;
 	f[1] = subtract;
@@ -36,8 +36,15 @@ int		main(int argc, char *argv[])
 	init(operations);
 	if (argc == 4 && valid(argv[2]))
 	{
-		operation = operations[ft_find_index(*argv[2], OPERATORS)];
-		ft_putnbr(operation(ft_atoi(argv[1]), ft_atoi(argv[3])));
+		if (*argv[2] == '/' && *argv[3] == '0')
+			ft_putstr("Stop : division by zero");
+		else if (*argv[2] == '%' && *argv[3] == '0')
+			ft_putstr("Stop : modulo by zero");
+		else
+		{
+			operation = operations[ft_find_index(*argv[2], OPERATORS)];
+			ft_putnbr(operation(ft_atoi(argv[1]), ft_atoi(argv[3])));
+		}
 		ft_putchar('\n');
 	}
 	else
